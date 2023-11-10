8b24050dcfab1431ed9626337be7b073125b84b1
Sallai András <andrewteki@gmail.com>
2023-11-10 13:07:37 +0100

    Az Sqlite osztály kész

--- src/App.java
+++ src/App.java
@@ -2,11 +2,13 @@ import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Mariadb mariadb = new Mariadb();
-        Connection conn = mariadb.connectDb();
+        // Mariadb mariadb = new Mariadb();
+        // Connection conn = mariadb.connectDb();
+        // conn.close();
+        Sqlite sqlite = new Sqlite();
+        Connection conn = sqlite.connectDb();
         conn.close();
         System.out.println("OK");
-        
         
     }
 }
